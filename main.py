5c3a512e6a22fcbc5eba1c4247870feb831a4f80
Prashant Srivastava <powprashant@gmail.com>
2016-11-05 00:08:16 +0530

    Update main.py
    commit after with actual deployment on Amazon Echo Dot.

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-
 ## Author : Prashant
 ## Learning Amazon Web Services( AWS) Lambda Functions with Amazon Echo
 
-class AlexaResponseBuilder:
+class AlexaResponseBuilder(object):
     def __init__(self, event, context ):
         self.event   = event
         self.context = context
         self.sessionAttributes = { }
-        self.sendToAlexa = {  
-   "version" : "1.0",
-   "sessionAttributes" :self.sessionAttributes,
-   "response":{  
-      "outputSpeech":{  
-         "type":"PlainText",
-         "text":" AlexaResponseBuilder - None",         
-      },
-      "card":{  
-         "type":"Simple",
-         "title":"AlexaResponseBuilder - Title",
-         "content":"AlexaResponseBuilder - Content",
-         "text":"AlexaResponseBuilder - Text",
-      },
-      "reprompt":{  
-         "outputSpeech":{  
-            "type":"Simple",
-            "text":"AlexaResponseBuilder - Text",
-      },
-      "shouldEndSession": False
-   }
+        self.sendToAlexa = {
+  "version": "1.0",
+  "response": {
+    "outputSpeech": {
+      "text": "Welcome to the Alexa Skills by P0W",
+      "type": "PlainText"
+    },
+    "shouldEndSession": False,
+    "reprompt": {
+      "outputSpeech": {
+        "text": "Please say some stock to listen current values",
+        "type": "PlainText"
       }
-   }
+    },
+    "card": {
+      "content": "Stock Values",
+      "type": "Simple",
+      "title": "Stock Values from Bombay Stock Exchange"
+    }
+  },
+  "sessionAttributes": {}
+}
 
     def run( self ):
         dispatcher = {
@@ -57,42 +55,97 @@ class AlexaResponseBuilder:
         return dispatcher()
         
     def onSessionEndedRequestReceived( self ):       
-        self.sendToAlexa['shouldEndSession'] = True
+        self.sendToAlexa['response']['shouldEndSession'] = True
         return self.sendToAlexa
 
     def onHelpIntentReceived( self ):        
-        self.sendToAlexa['shouldEndSession'] = True
+        self.sendToAlexa['response']['shouldEndSession'] = True
         return self.sendToAlexa
 
     def onCancelIntentReceived( self ):
         self.sendToAlexa['response'][ 'outputSpeech' ][ 'text' ] = \
                     "Thank you for trying the P0W Alexa Skills Kit. " \
                     "Have a nice day! "         
-        self.sendToAlexa['shouldEndSession'] = True
+        self.sendToAlexa['response']['shouldEndSession'] = True
         return self.sendToAlexa
 
     def onStopIntentReceived( self ):
         self.sendToAlexa['response'][ 'outputSpeech' ][ 'text' ] = \
                     "Thank you for trying the P0W Alexa Skills Kit. " \
                     "Have a nice day! "         
-        self.sendToAlexa['shouldEndSession'] = True
+        self.sendToAlexa['response']['shouldEndSession'] = True
         return self.sendToAlexa
 
     def onCustomIntentReceived( self ):
         return self.sendToAlexa
 
 
+#python 2.7.6
+from urllib import *
+import json
+import time
+import copy
 
+class GoogleFinanceStockGrabber(object):
+    def __init__(self):
+        self.prefix = "http://finance.google.com/finance/info?client=ig&q="
+
+    def get(self,symbol):
+        url = self.prefix+"%s"%(symbol)
+        u = urlopen(url)
+        content = u.read().decode('utf-8')
+        
+        obj = json.loads(content[3:])
+        return obj
+
+    def stockPrinter( self, symb ):
+        print ( '\n%10s | %-6s : %10s (%8s %% ) < %s >' % (symb['t'], symb['e'], symb['l'], symb['cp'], symb['lt'] ) )
+          
+class StockRunner(  GoogleFinanceStockGrabber ):
+    def __init__(self, AllStocks):
+        super(StockRunner, self).__init__()
+        self.AllStocks = copy.deepcopy(AllStocks)
+            
+    def run(self):
+        res = []
+        quote = self.get(','.join( self.AllStocks.keys() ) )
+        for symb in quote:
+            stockKey = '%s:%s' % ( symb['e'], symb['t'] )
+            res.append ( '%s is %s Rupees' % (self.AllStocks[stockKey], symb['l'] ) )
+        return "\n ..".join(res)
+               
+        
+    
+
+AllStocks = { 
+               ## 'Exchange:Symbol' : Qty to buy
+                  'BOM:509480'      : 'Berger Paints'             ,
+                  'BOM:500233'      : 'Kajaria Ceramics'          ,
+                  'BOM:532540'      : 'Tata Consultancy Services' ,
+                  'BOM:532500'      : 'Maruti Suzuki Limited'     ,
+                  'BOM:500096'      : 'Dabur India Limited'       ,
+                  
+                }
+                
 class GreetFromAlexa( AlexaResponseBuilder ):
     def __init__(self, event, context ):
         super( GreetFromAlexa, self ).__init__( event, context )
+        self.sockrunner = StockRunner(AllStocks)
 
     def onLaunchRequestReceived( self ):
+        print ('LAUCH REQUEST')
         self.sendToAlexa['response'][ 'outputSpeech' ][ 'text' ] = \
                     "Welcome to the P0W Alexa Skills Kit Test. " \
                     "This is developed by Prashant"
+        self.sendToAlexa['response']['shouldEndSession'] = False
         return  self.sendToAlexa
-
+        
+    def onCustomIntentReceived( self ):
+        print ('INTENT REQUEST' )
+        self.sendToAlexa['response'][ 'outputSpeech' ][ 'text' ] = \
+                    "Share Value from Bombay Stock Exchage as follows :\n %s " % self.sockrunner.run()
+        self.sendToAlexa['response']['shouldEndSession'] = True
+        return  self.sendToAlexa
 
 def lambda_handler( event, context ):
     '''
@@ -100,4 +153,3 @@ def lambda_handler( event, context ):
     '''    
     alexaObj = GreetFromAlexa(  event, context )
     return alexaObj.run( )
-
